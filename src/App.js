b80f0915e1defff24cddb1eb244613a0ebe722a0
kamamile-tea <kamille.tipan@gmail.com>
2020-06-17 20:21:21 -0400

    edited image and made changes to the left-panel

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import LoginRegister from "./pages/LoginRegister";
 
 //Style
 import "./App.css";
+import Typography from '@material-ui/core/Typography';
+
 
 class App extends React.Component {
   render() {
@@ -19,7 +21,20 @@ class App extends React.Component {
         <NavBar />
         <Grid container direction="column">
           <Grid item container>
-            <Grid item xs={6} className="panels leftPanel" />
+          <Grid item container xs={6} className="panels leftPanel">
+
+              <img src={require('./images/collaboration.jpg')} className = "leftPanelImg"/>
+              <Grid item xs={2}></Grid>
+              <Grid item xs={8}>
+                <div>
+                  <Typography variant='h5' gutterbottom className = "missionTitle">JOIN A COLLABORATIVE COMMUNIT
+                    Y OF PROBLEM SOLVERS</Typography>
+                </div>
+              </Grid>
+              <Grid item xs={2}></Grid>
+
+
+       </Grid>
             <Grid item xs={6} className="panels">
               <Switch>
                 <Route path="/about" component={About} />
