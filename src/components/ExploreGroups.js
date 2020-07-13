b2811e8887c987a88fa4dc6f288158fa0911f21a
Jason Yuan <jasony123123@gmail.com>
2020-07-13 00:56:03 -0500

    working explore view

--- src/components/ExploreGroups.js
+++ src/components/ExploreGroups.js
@@ -1,16 +1,48 @@
 import React from "react";
 
+import { Input, InputBase } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+
 import ExploreGroupsCard from "./ExploreGroupsCard";
+import "../styles/searchBar.css";
 
 class ExploreGroups extends React.Component {
   render() {
     return (
-      <div>
-        <h1> This is the Explore Groups </h1>
-        <ExploreGroupsCard />
-        <ExploreGroupsCard />
-        <ExploreGroupsCard />
-      </div>
+      <Grid container spacing={5} justify="center" alignItems="center">
+        <Grid item xs={12} />
+        <Grid item xs={12} />
+        <Grid item xs={6}>
+          <div className="searchBar">
+            <InputBase fullWidth placeholder=" Find your next group..." />
+          </div>
+        </Grid>
+        <Grid item xs={1}>
+          <IconButton variant="contained" color="primary">
+            <SearchIcon />
+          </IconButton>
+        </Grid>
+        <Grid item xs={8}>
+          <ExploreGroupsCard />
+        </Grid>
+        <Grid item xs={8}>
+          <ExploreGroupsCard />
+        </Grid>
+        <Grid item xs={8}>
+          <ExploreGroupsCard />
+        </Grid>
+        <Grid item xs={8}>
+          <ExploreGroupsCard />
+        </Grid>
+        <Grid item xs={8}>
+          <ExploreGroupsCard />
+        </Grid>
+        <Grid item xs={8}>
+          <ExploreGroupsCard />
+        </Grid>
+      </Grid>
     );
   }
 }
--- src/components/ExploreGroupsCard.js
+++ src/components/ExploreGroupsCard.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
 
-class ExploreGroupsCard extends React.Component {
-	render() {
-		return <h1> This is a card for explore groups </h1>;
-	}
+function ExploreGroupsCard(props) {
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Grid container direction="row" alignItems="center">
+          <Grid item xs={8} spacing={1} container>
+            <Grid item xs={12}>
+              <Typography variant="h4">
+                <strong>Machine Learning, Coursera </strong>
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5">
+                <strong> June 6, 2020 - September 10, 2020 </strong>
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">
+                <strong>15/20 enrolled </strong>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={4}>
+            <Grid container direction="column" alignItems="center" spacing={2}>
+              <Grid item xs={1} />
+              <Grid item xs={12}>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="secondary"
+                  disableElevation
+                >
+                  Enroll In Course
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography>
+                  <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                    Visit Course
+                  </Link>
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default ExploreGroupsCard;
