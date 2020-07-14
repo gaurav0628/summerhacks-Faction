1a87e9cba50f72c6475cd9f5b7b3f5da2ca4025b
kamamile-tea <kamille.tipan@gmail.com>
2020-07-14 17:18:11 -0400

    Profile page update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+
+import "../styles/dashbar.css";
+import AvatarBubble from "./AvatarBubble";
+import ProfileTextBox from "./ProfileTextBox";
 
 class Profile extends React.Component {
   render() {
-    return <h1> This is the profile page </h1>;
+    return(
+    <Grid container>
+      <Grid container xs={3}>
+      <img
+        src={require("../images/People-with-Activity.jpg")}
+        class = "background-panel"
+      />
+      </Grid>
+      <Grid container direction="column" xs={6}>
+        <Grid item>
+          <AvatarBubble></AvatarBubble>
+        </Grid>
+        <Grid item>
+          <ProfileTextBox>Name</ProfileTextBox>
+          <ProfileTextBox>Name</ProfileTextBox>
+          <ProfileTextBox>Name</ProfileTextBox>
+          <ProfileTextBox>Name</ProfileTextBox>
+        </Grid>
+      </Grid>
+      <Grid container xs={3}>
+      <img
+        src={require("../images/People-with-Activity.jpg")}
+        class = "background-panel"
+      />
+      </Grid>
+    </Grid>
+    )
   }
 }
 
