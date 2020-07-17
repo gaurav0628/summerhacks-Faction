06658e38990bead94b61de091bfb07f0e6844ff6
Jason Yuan <jasony123123@gmail.com>
2020-07-17 15:02:00 -0500

    create group form

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -1,15 +1,116 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 
 class CreateGroup extends React.Component {
-	render() {
-		return (
-			<h1>
-				{" "}
-				Form to create a new group. On submit, should also auto register
-				the user and go back to explore groups
-			</h1>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      courseName: "",
+      link: "",
+      startDate: "",
+      endDate: "",
+      aList: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  handleSubmit(event) {
+    const data = this.state;
+    alert(data.courseName);
+    alert(data.link);
+    alert(data.startDate);
+    alert(data.endDate);
+    alert(data.aList);
+    this.setState({
+      courseName: "",
+      link: "",
+      startDate: "",
+      endDate: "",
+      aList: "",
+    });
+  }
+  render() {
+    const data = this.state;
+    return (
+      <Grid container direction="row" justify="center" spacing={2}>
+        <Grid item xs={12} />
+        <Grid item xs={12} />
+        <Grid item xs={7}>
+          <TextField
+            type="text"
+            onChange={this.handleChange}
+            fullWidth
+            variant="outlined"
+            label="Name of Course"
+            name="courseName"
+            value={data.courseName}
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <TextField
+            type="text"
+            onChange={this.handleChange}
+            fullWidth
+            variant="outlined"
+            label="Link to Course"
+            name="link"
+            value={data.link}
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <TextField
+            type="text"
+            onChange={this.handleChange}
+            fullWidth
+            variant="outlined"
+            label="Start Date"
+            name="startDate"
+            value={data.startDate}
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <TextField
+            type="text"
+            onChange={this.handleChange}
+            fullWidth
+            variant="outlined"
+            label="End Date"
+            name="endDate"
+            value={data.endDate}
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <TextField
+            type="text"
+            onChange={this.handleChange}
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={1}
+            rowsMax={50}
+            label="Enter all assignments (one per line)"
+            name="aList"
+            value={data.aList}
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.handleSubmit}
+            fullWidth
+          >
+            Create Course
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default CreateGroup;
