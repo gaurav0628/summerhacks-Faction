9623b618d65811a6037d6da6c560e6f3e9fb21c1
Jason Yuan <jasony123123@gmail.com>
2020-08-12 18:44:36 -0500

    can enroll now

--- src/components/ExploreGroupsCard.js
+++ src/components/ExploreGroupsCard.js
@@ -5,10 +5,11 @@ import Typography from "@material-ui/core/Typography";
 import { Grid } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
-
+import KEYS from "../keys.js";
+  
 function ExploreGroupsCard(props) {
   // We can make it so it outputs cuter later I guess.
-  function date(str){
+  function date(str) {
     var correct = str.substring(0, str.indexOf("T"));
     return correct;
   }
@@ -21,8 +22,49 @@ function ExploreGroupsCard(props) {
   var memberEmails = props.member_list_emails;
   var memberNames = props.member_list_names;
 
-  function handleClick(){
-    alert("enrooll");
+  function handleClick() {
+    var axios = require("axios");
+    var qs = require("qs");
+    var data = qs.stringify({
+      course_name: props.course_name,
+      date_started: props.date_started,
+      expected_end_date: props.expected_end_date,
+      first_name: localStorage.getItem("first_name"),
+      last_name: localStorage.getItem("last_name"),
+      email: localStorage.getItem("email"),
+    });
+    var config = {
+      method: "post",
+      url: KEYS.APIURL + "/groups-api/enrollGroups",
+      headers: {
+        "x-auth-token": localStorage.getItem("auth-token"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: data,
+    };
+
+    console.log(config);
+    
+    axios(config)
+      .then(function (response) {
+        alert("Success");
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        alert("error");
+        if (error.response) {
+          console.log("ERROR: Request made; server responded");
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log("ERROR: Request made; No response received");
+          console.log(error.request);
+        } else {
+          console.log("ERROR: Setting up the request triggered an Error");
+          console.log(error.message);
+        }
+      });
   }
   return (
     <Card variant="outlined">
@@ -36,7 +78,10 @@ function ExploreGroupsCard(props) {
             </Grid>
             <Grid item xs={12}>
               <Typography variant="h5">
-                <strong> {start} -> {end} </strong>
+                <strong>
+                  {" "}
+                  {start} -> {end}{" "}
+                </strong>
               </Typography>
             </Grid>
           </Grid>
@@ -56,9 +101,7 @@ function ExploreGroupsCard(props) {
               </Grid>
               <Grid item xs={12}>
                 <Typography>
-                  <Link href= {link}>
-                    Visit Course
-                  </Link>
+                  <Link href={link}>Visit Course</Link>
                 </Typography>
               </Grid>
             </Grid>
